67465bbe72ccfbef9c2ae1de01715231b7ebe9ce
Venuri <venurivethmini2005@gmail.com>
2025-10-15 17:33:19 +0530

    Added manage cities method

--- src/LogisticManagementSystem.java
+++ src/LogisticManagementSystem.java
@@ -11,7 +11,7 @@ public class LogisticManagementSystem {
             printMenu();
             System.out.print("Enter your choice: ");
             choice = sc.nextInt();
-
+            sc.nextLine(); //I used this to prevent unnecessary errors.
             switch (choice) {
                 case 1:
                     manageCities();
@@ -45,10 +45,38 @@ public class LogisticManagementSystem {
     }
 
     public static void manageCities(){
-        
+        int option;
+        do{
+            System.out.println("\n1. Add a new city");
+            System.out.println("2. Rename a city");
+            System.out.println("3. Remove a city");
+            System.out.println("4. Stop City Managing");
 
+            System.out.print("\nSelect your choice: ");
+            option = sc.nextInt();
+            sc.nextLine();
 
+            switch(option){
+                case 1:
+                    addCity();
+                    break;
+                case 2:
+                    renameCity();
+                    break;
+                case 3:
+                    removeCity();
+                    break;
+                case 4:
+                    break;
+                default:
+                    System.out.println("Invalid choice.");
+
+            }
+
+        }while (option!=4);
     }
+
+    
 
     public static void manageDistances(){
 
@@ -59,6 +87,9 @@ public class LogisticManagementSystem {
     }
 
     public static void viewReports(){
+        for (int i=0; i<cities.length; i++){
+                System.out.println(cities[i]);
+            }
 
     }
 
