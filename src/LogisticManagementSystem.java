a9bbbea2c454e4b116601469c42babc62469c433
Venuri <venurivethmini2005@gmail.com>
2025-10-17 21:28:35 +0530

    Added distance management method

--- src/LogisticManagementSystem.java
+++ src/LogisticManagementSystem.java
@@ -2,8 +2,12 @@ import java.util.Scanner;
 
 public class LogisticManagementSystem {
     static Scanner sc = new Scanner (System.in);
-    static String cities [] = new String [30];
-    static int cityCount = 0; //to maintain an easier array
+    static final int MAX_CITIES = 30;
+    static String [] cities = new String [MAX_CITIES];
+    static int cityCount = 0; //to maintain an easier array handling
+    
+    static int[][] distance = new int[MAX_CITIES][MAX_CITIES];
+    //static int indexOfcity;
     
     public static void main(String[] args) {
         
@@ -36,7 +40,7 @@ public class LogisticManagementSystem {
     }
 
     public static void printMenu(){
-        System.out.println("\n===== Logistics Management System =====");
+        System.out.println("\n\n===== Logistics Management System =====");
             System.out.println("1. Manage Cities");
             System.out.println("2. Manage Distances");
             System.out.println("3. Handle Deliveries");
@@ -48,12 +52,12 @@ public class LogisticManagementSystem {
     public static void manageCities(){
         int option;
         do{
-            System.out.println("\n1. Add a new city");
-            System.out.println("2. Rename a city");
-            System.out.println("3. Remove a city");
-            System.out.println("4. Stop City Managing");
+            System.out.println("\n\t1. Add a new city");
+            System.out.println("\t2. Rename a city");
+            System.out.println("\t3. Remove a city");
+            System.out.println("\t4. Stop City Managing");
 
-            System.out.print("\nSelect your choice: ");
+            System.out.print("\n\tSelect your choice: ");
             option = sc.nextInt();
             sc.nextLine();
 
@@ -86,7 +90,7 @@ public class LogisticManagementSystem {
 
         System.out.print("City Name: ");
         String city = sc.nextLine().toUpperCase();
-
+        
         //to make sure the city is unique
         for (int i=0; i<cityCount; i++){
             if (cities[i].equals(city)){
@@ -117,8 +121,6 @@ public class LogisticManagementSystem {
                 System.out.println("City not found!");
             }
         }
-            
-
     }
 
     public static void removeCity(){
@@ -140,15 +142,86 @@ public class LogisticManagementSystem {
     }
 
     public static void manageDistances(){
+        if (cityCount <2){
+            System.out.println("There should be two cities.");
+            return;
+        }
 
+        while (true){
+            System.out.println("\n\t1. Add or Edit distance");
+            System.out.println("\t2. Display distance table");
+            System.out.println("\t3. Stop distance management");
+
+            System.out.print("\tEnter your choice: ");
+            int choice = sc.nextInt();
+
+            if (choice ==1){
+                System.out.println("\nAvailable cities: ");
+                for (int i=0; i<cityCount; i++){
+                    System.out.println(i+": "+ cities[i]);
+                }
+                System.out.print("\nEnter starting city index: ");
+                int from = sc.nextInt();
+
+                if (from<cityCount){
+                    System.out.print("\nEnter destination city index: ");
+                    int to = sc.nextInt();
+                        if(to<cityCount){
+                            if (from==to){
+                            System.out.println("Distance from a city to itself is 0!");
+                            distance [from][to]=0;
+                            continue;
+                            }
+
+                        System.out.print("Enter distance in km: ");
+                        int dist=sc.nextInt();
+
+                        distance[from][to]=dist;
+                        distance[to][from]=dist;
+                        System.out.println("Distance updated successfully!");
+                        }else{
+                            System.out.println("Invalid city index!");
+                        }
+                }else{
+                    System.out.println("Invalid city index!");
+                }
+
+            }else if(choice==2){
+                System.out.println("\nDistance Table (km)");
+                System.out.printf("%-15s","");
+                for (int i = 0; i < cityCount; i++) {
+                    System.out.printf("%-15s",cities[i]);
+                }
+            System.out.println();
+
+            for (int i = 0; i < cityCount; i++) {
+                System.out.printf("%-15s",cities[i]);
+                for (int j = 0; j < cityCount; j++) {
+                    System.out.printf("%-15s",distance[i][j]);
+                }
+                System.out.println();
+            }
+          }else if (choice ==3){
+            return;
+          }else{
+            System.out.println("Invalid choice. Try again.");
+          }
+        }
     }
+
+    public static void vehicleManagement(){
+        final int VAN [] = {1000,30,60,12};
+        final int TRUCK [] = {5000,40,50,6};
+        final int LORRY []= {10000,80,45,4};
+
+    }                                          
 
     public static void handleDeliveries(){
 
     }
 
     public static void viewReports(){
-        for (int i=0; i<cities.length; i++){
+        for (int i=0; i<cityCount; i++){
                 System.out.println(cities[i]);
             }
 
